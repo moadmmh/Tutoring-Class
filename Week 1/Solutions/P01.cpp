7f4ee286f792a832367b3c8d8476313816ef75d4
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-20 22:56:25 +0800

    Create P01.cpp

---
+++ Week 1/Solutions/P01.cpp
@@ -0,0 +1,14 @@
+//Solution for the first problem (Mekki the youngest programmer)
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    
+    return 0;
+}
