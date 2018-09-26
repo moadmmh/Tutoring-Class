cf8e63597608bf7f915e69db1948863238192e79
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-26 23:30:29 +0800

    Create Problem 1.cpp

---
+++ Week 1/Solutions/Tutoring Class/Problem 1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    
+    char c;
+    cout<<"Please Enter your character"<<endl;
+    cin>>c;
+    if(c>='A' && c<='Z'){
+        if(c=='A' || c=='E' || c=='U' || c=='I' || c=='O')
+            cout<<"Upper case and Vowel"<<endl;
+        else
+            cout<<"Upper case and Consonant"<<endl;
+    }
+    else if(c>='A' && c<='Z'){
+        if(c=='a' || c=='e' || c=='u' || c=='i' || c=='o')
+            cout<<"Lower case and Vowel"<<endl;
+        else
+            cout<<"Lower case and Consonant"<<endl;
+    }
+    else if(c>='0' && c<='9'){
+        cout<<"Digit"<<endl;
+    }
+    else {
+        cout<<"Unknown character"<<endl;
+    }
+    return 0;
+}
